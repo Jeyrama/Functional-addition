bd828112bbd2b1a20828fb2fd83c1d1b34a0ced6
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-07-22 17:17:41 -0700

    Create Functional addition.js

---
+++ Functional addition.js
@@ -0,0 +1,10 @@
+/*
+Create a function add(n)/Add(n) which returns a function that always adds n to any number.
+
+Examples:
+  let addOne = add(1);
+  addOne(3); // 4
+
+  let addThree = add(3);
+  addThree(3); // 6
+*/
